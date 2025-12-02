72fc6b618a2b067ad1012a8c1d91f3156d73f51b
William Mosquera <wjmmk3@gmail.com>
2025-12-02 16:06:57 -0500

    feat: Initial load.

---
+++ src/app/auth/reset-password/reset-password.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [],
+  templateUrl: './reset-password.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ResetPassword { }
