3e850b2f91f67719f92cc67d3f02c4b894a51e9e
William Mosquera <wjmmk3@gmail.com>
2025-12-04 14:21:28 -0500

    feat: Routes and httpClient module configured.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import routes from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
+import { loggingInterceptor } from './shared/interceptors/login.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor, loggingInterceptor])
+    ),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./auth/home/home').then(m => m.Home),
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
+  },
+
+  /* {
+    path: 'pets',
+    loadChildren: () =>
+      import('./pets/pets.routes').then(m => m.PETS_ROUTES),
+  },
+
+  {
+    path: 'health',
+    loadChildren: () =>
+      import('./health/health.routes').then(m => m.HEALTH_ROUTES),
+  },
+
+  {
+    path: 'nutrition',
+    loadChildren: () =>
+      import('./nutrition/nutrition.routes').then(m => m.NUTRITION_ROUTES),
+  },
+
+  {
+    path: 'reminders',
+    loadChildren: () =>
+      import('./reminders/reminders.routes').then(m => m.REMINDERS_ROUTES),
+  },
+ */
+  // fallback
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+export default appRoutes;
