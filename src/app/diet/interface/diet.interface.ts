6005141f3e72cca3410bfacbabf79497b9a477c2
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-04 00:28:55 -0300

    servicios de dieta

---
+++ src/app/diet/interface/diet.interface.ts
@@ -0,0 +1,9 @@
+    export interface IDiet{
+    dietaId?: number,
+    codigo: string,
+    porcionDia: number,
+    notas: string,
+    mascotaId: number,
+    tipoAlimentoId: number,
+    unidadMedidaId: number
+}
