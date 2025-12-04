63f7d5403be95160538e1527acdc3cd36ca41986
Gabriela1994 <gabrielleaurevoir@gmail.com>
2025-12-03 21:17:10 -0300

    servicios desparacitaciones

---
+++ src/app/deworming/interface/deworming.interface.ts
@@ -0,0 +1,9 @@
+export interface IDeworming {
+    desparacitacionId?: number,
+    codigo?: string,
+    fechaAplicacion: string,
+    fechaProxima: string,
+    notas: string,
+    mascotaId: number,
+    tipoDesparacitacionId: number
+}
