99b41de27670e514e31d6d5020edb3cdba51ab27
William Mosquera <wjmmk3@gmail.com>
2025-12-06 16:25:01 -0500

    fix: Home, Login and Register components done.

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -57,8 +57,7 @@ export class Register {
       nombre: ['', [Validators.required, Validators.minLength(2)]],
       apellido: ['', [Validators.required, Validators.min(2)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      telefono: ['', [Validators.required]]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
